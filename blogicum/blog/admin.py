4e988f4a087b1f8de878e80f49beef3efb8d44ce
Denis Ivanov <dnb8866@gmail.com>
2024-10-01 17:10:18 +0700

    for review, iteration 2

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from blog.models import Post, Category, Location
+from blog.models import Post, Category, Location, Comment
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
         'title',
         'short_text',
         'pub_date',
@@ -13,14 +14,12 @@ class PostAdmin(admin.ModelAdmin):
         'location',
         'is_published',
         'created_at'
-    ]
-    list_editable = [
-        'is_published'
-    ]
+    )
+    list_editable = ('is_published',)
     search_fields = ('title', 'text')
     list_filter = ('is_published', 'pub_date', 'category', 'location')
     list_display_links = ('title',)
-    ordering = ['-pub_date', 'author']
+    ordering = ('-pub_date', 'author')
     list_per_page = 10
 
     @admin.display(description='text')
@@ -28,36 +27,46 @@ class PostAdmin(admin.ModelAdmin):
         return post.text[:50]
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
         'title',
         'description',
         'slug',
         'is_published',
         'created_at'
-    ]
-    list_editable = [
-        'is_published'
-    ]
+    )
+    list_editable = ('is_published',)
     search_fields = ('title', 'description', 'slug')
     list_display_links = ('title',)
     list_filter = ('is_published', 'created_at')
 
 
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = [
+    list_display = (
         'name',
         'is_published',
         'created_at'
-    ]
-    list_editable = [
-        'is_published'
-    ]
+    )
+    list_editable = ('is_published',)
     list_display_links = ('name',)
     search_fields = ('name',)
     list_filter = ('is_published', 'created_at')
 
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Location, LocationAdmin)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'short_text',
+        'author',
+        'created_at',
+        'post',
+    )
+    list_display_links = ('short_text',)
+    search_fields = ('text',)
+    list_filter = ('created_at',)
+
+    @admin.display(description='text')
+    def short_text(self, comment):
+        return comment.text[:50]
